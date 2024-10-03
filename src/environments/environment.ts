81509dad44df17a6bb1c5e9491e728c6e5f3f4cc
DAVV422 <diegoalberto42216@gmail.com>
2024-10-03 03:44:15 -0400

    fix baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl: 'https://database-diagram-back.onrender.com/',
+  baseUrl: 'https://database-diagram-back.onrender.com/api',
   baseSocket: 'http://localhost:3001',
   production: false,
 };
